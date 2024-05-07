5f09f1744569d5e4c739434b48e468e9b0ada576
sinajr <jamyarisina@gmail.com>
2024-05-07 13:29:23 +0300

    adding images and starting slider section

--- javascript/java.js
+++ javascript/java.js
@@ -1,1 +1,14 @@
 var $=document;
+let sliderMainPic=[
+    slider="image/slider/shopping-concept-close-up-portrait-young-beautiful-attractive-redhair-girl-smiling-looking-camera.jpg",
+    slider="image/slider/young-man-holding-shopping-bags-showing-mobile-phone-screen-money-application-standing-yell.jpg",
+    slider="image/slider/95076.jpg",
+    slider="image/slider/shopping-concept-close-up-portrait-young-beautiful-attractive-redhair-girl-smiling-looking-camera-smaller.jpg"
+]
+console.log(sliderMainPic)
+let slider=document.getElementById('slider')
+let index=0
+function nextSlide(){
+    index++
+    slider.src=sliderMainPic[index]
+}
