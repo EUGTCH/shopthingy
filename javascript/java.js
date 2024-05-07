139cc3e122f992c23a1de7b1ee71e7a7c79b93c5
sinajr <jamyarisina@gmail.com>
2024-05-07 13:46:36 +0300

    slider v1 is complited

--- javascript/java.js
+++ javascript/java.js
@@ -1,14 +1,26 @@
 var $=document;
 let sliderMainPic=[
-    slider="image/slider/shopping-concept-close-up-portrait-young-beautiful-attractive-redhair-girl-smiling-looking-camera.jpg",
-    slider="image/slider/young-man-holding-shopping-bags-showing-mobile-phone-screen-money-application-standing-yell.jpg",
-    slider="image/slider/95076.jpg",
-    slider="image/slider/shopping-concept-close-up-portrait-young-beautiful-attractive-redhair-girl-smiling-looking-camera-smaller.jpg"
+    sliderPic="image/slider/shopping-concept-close-up-portrait-young-beautiful-attractive-redhair-girl-smiling-looking-camera.jpg",
+    sliderPic="image/slider/young-man-holding-shopping-bags-showing-mobile-phone-screen-money-application-standing-yell.jpg",
+    sliderPic="image/slider/95076.jpg"
 ]
-console.log(sliderMainPic)
 let slider=document.getElementById('slider')
 let index=0
+slider.src=sliderMainPic[index];
+slider.classList.add('slider-animation');
+slider.addEventListener('animationend',()=>{
+slider.classList.remove('slider-animation');
+})
 function nextSlide(){
+    console.log(index)
     index++
-    slider.src=sliderMainPic[index]
+        slider.classList.add('slider-animation');
+        slider.addEventListener('animationend',()=>{
+        slider.classList.remove('slider-animation');
+        })
+    slider.src=sliderMainPic[index];
+    if(index>=sliderMainPic.length-1){
+       return index=0;
+    }
 }
+setInterval(nextSlide,5000)
